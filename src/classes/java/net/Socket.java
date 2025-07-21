ceeaa9c363b179722eb356e0cc4bdb331a77f682
Mahmoud Khawaja <mahmoud.khawaja97@gmail.com>
2025-07-21 14:40:49 +0300

    use direct implementation

--- src/classes/java/net/Socket.java
+++ src/classes/java/net/Socket.java
@@ -65,7 +65,9 @@ public class Socket implements java.io.Closeable {
   }
 
   private native void connect(String host, int port) throws IOException;
-  public native int getHash();
+  public int getHash() {
+    return this.hash;
+  }
   public void connect(SocketAddress endpoint) throws IOException {
     connect(endpoint, 0);
   }
